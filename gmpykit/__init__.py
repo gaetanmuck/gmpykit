3a9a3c6a8d7d7ca168bdbe781f7b8b863eb4bd67
Gaétan Muck <gaetan.muck@gmail.com>
2024-07-20 08:22:03 +0200

    correction

--- gmpykit/__init__.py
+++ gmpykit/__init__.py
@@ -10,6 +10,7 @@ from .colors import get_random_color
 from .dates import to_julian_day, from_julian_day, parse_date_str_formated, parse_date_tuple_formated
 from .eta import Eta
 from .file import read_pickle, write_pickle, read_json, write_json
+from .llm import ask_llm
 from .math import binomial_nb
 from .execution import deco_interval, set_interval
 from .timestamps import now, format_duration
--- gmpykit/llm.py
+++ gmpykit/llm.py
@@ -1,18 +1,11 @@
 import requests
 import json
 
-ollama_url = 'http://localhost:11434/api/generate'
-ollama_llm_name = 'llama3'
+default_url = 'http://localhost:11434/api/generate'
+default_llm_name = 'llama3'
 
-def init(url:str, llm_name: str):
-    global ollama_url, ollama_llm_name
-    ollama_url = url
-    ollama_llm_name = llm_name
-
-
-def ask(prompt):
-    global ollama_url, ollama_llm_name
-    response = requests.post(ollama_url, json={'model': ollama_llm_name, 'prompt': prompt, 'option': {'temperature': 0}})
+def ask_llm(prompt, ollama_server_url='http://localhost:11434/api/generate', ollama_llm_name='llama3'):
+    response = requests.post(ollama_server_url, json={'model': ollama_llm_name, 'prompt': prompt, 'option': {'temperature': 0}})
     text = response.text.strip()
     lines = text.split('\n')
     tokens = list(map(lambda line: json.loads(line)['response'], lines))
